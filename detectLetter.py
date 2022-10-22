b3df9ca4ab4dbf6f90a915da941f8d3dccb9c9c2
Kunal2341 <kunal.aneja101@gmail.com>
2022-10-22 12:22:46 -0400

    google letter code

--- detectLetter.py
+++ detectLetter.py
@@ -2,8 +2,8 @@ from PIL import Image
 import pytesseract
 import numpy as np
 
-pytesseract.pytesseract.tesseract_cmd = "C:/Users/kunal/Anaconda3/envs/hackgt-9/Scripts/pytesseract.exe"
-
+pytesseract.pytesseract.tesseract_cmd = (r"C:/Users/kunal/Anaconda3/envs/hackgt-9/Scripts/pytesseract.exe")
+pytesseract.pytesseract.tesseract_cmd = "C:\Program Files\Tesseract-OCR\pytesseract.exe"
 
 filename = 'letterImageDetect.png'
 img1 = Image.open(filename)
---
+++ detectLetterGoogle.py
@@ -0,0 +1,38 @@
+from google.oauth2 import service_account
+import time
+start_time = time.time()
+credentials = service_account.Credentials.from_service_account_file("hackgt-366316-0c3450bdab27.json")
+def detect_text(path):
+    """Detects text in the file."""
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient(credentials=credentials)
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+
+
+
+detect_text("ex-images/1.jpg")
+print(time.time() - start_time)

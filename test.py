8c022ab8e815ed0f8fd6764fd1c057e681d58041
Kunal2341 <kunal.aneja101@gmail.com>
2022-10-22 09:59:05 -0400

    other final changes clean up

--- test.py
+++ test.py
@@ -1,52 +1,49 @@
-import cv2
-import os 
-import numpy as np
-from matplotlib import pyplot as plt
-
-imagesFolder = "ex-images"
-example_img = os.path.join(imagesFolder, "1.jpg")
-
-
-#List of xy and width 
-
-img = cv2.imread(example_img)
-
-# converting image into grayscale image
-scale_percent = 30
-width = int(img.shape[1] * scale_percent / 100)
-height = int(img.shape[0] * scale_percent / 100)
-dsize = (width, height)
-# resize image
-img = cv2.resize(img, dsize)
-
 def intersects(box1, box2):
     return not (box1[2] < box2[0] or box1[0] > box2[2] or box1[1] > box2[3] or box1[3] < box2[1])
 
-# Get image from between 2 main colors 
-imghsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-lower_blue = np.array([25,25,0]) 
-upper_blue = np.array([255,255,255])
-mask_blue = cv2.inRange(imghsv, lower_blue, upper_blue)
-#Show masked image
-# cv2.imshow("winname" , mask_blue)
-contours, _ = cv2.findContours(mask_blue, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+def get_coordinates(path_to_image: str):
+    import cv2
+    import numpy as np
+    """Returns the coordinates of the shapes in the image"""
+    LOWER_BLUE_COLOR = [25,25,0]
+    UPPER_BLUE_COLOR = [255,255,255]
+    CONTOUR_SIZE_RESTRICTION = 40
+    BORDER_SHAPE_ADD = 15
 
-imKnown = np.copy(img)
+    img = cv2.imread(path_to_image)
+    imghsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower_blue = np.array(LOWER_BLUE_COLOR) 
+    upper_blue = np.array(UPPER_BLUE_COLOR)
+    mask_blue = cv2.inRange(imghsv, lower_blue, upper_blue)
+    #Show masked image
+    # cv2.imshow("winname" , mask_blue)
+    contours, _ = cv2.findContours(mask_blue, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-x, y, w, h = 220, 441, 26, 29
-cv2.rectangle(imKnown,(x,y),(x+w,y+h),(255,0,0),2)
-cv2.putText(imKnown,str("orginal"),(x+w+10,y+h),0,0.3,(255,0,0))
+    im = np.copy(img)
+    imSizeRestrict = np.copy(img)
+    dimensionsShapes = []
+    for c in contours:
+        if (cv2.contourArea(c) > CONTOUR_SIZE_RESTRICTION): 
+            rect = cv2.boundingRect(c)
+            x,y,w,h = rect
+            dimensionsShapes.append([x,y,w,h])
+    # Checks each value with all others in arrary to see intersection, removes smaller array
+    for shape in dimensionsShapes:
+        for shapeCheck in dimensionsShapes:
+            if intersects((shape[0],shape[1],shape[2]+shape[0],shape[3]+shape[1]), (shapeCheck[0],shapeCheck[1],shapeCheck[2]+shapeCheck[0],shapeCheck[3]+shapeCheck[1])) and shape != shapeCheck:
+                if shape[2]*shape[3] > shapeCheck[2]*shapeCheck[3]:
+                    dimensionsShapes.remove(shapeCheck)
+                else:
+                    dimensionsShapes.remove(shape)
 
-x, y, w, h = 83, 440, 146, 174
-cv2.rectangle(imKnown,(x,y),(x+w,y+h),(0,255,0),2)
-cv2.putText(imKnown,str("intersection"),(x+w+10,y+h),0,0.3,(255,0,0))
+    textBuffer = 5
 
+    bufferedDimensions = []
 
-x, y, w, h = 83, 440, 229, 614
-cv2.rectangle(imKnown,(x,y),(x+w,y+h),(0,0,255),2)
-cv2.putText(imKnown,str("final"),(x+w+10,y+h),0,0.3,(255,0,0))
+    for shapeFinalizaed in dimensionsShapes:
+        x, y, w, h = shapeFinalizaed
+        bufferedDimensions.append([x - BORDER_SHAPE_ADD, y - BORDER_SHAPE_ADD,
+            w + BORDER_SHAPE_ADD * 2, h + BORDER_SHAPE_ADD * 2])
 
-
-cv2.imshow('Known', imKnown)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    return bufferedDimensions
+print(get_coordinates("ex-images/1.jpg"))

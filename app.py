5d78eace0493cc81e4b85accf71d73212b9b98e6
Richard Rex <richardr2926@gmail.com>
2022-10-22 14:02:49 -0400

    removed duplicate

--- app.py
+++ app.py
@@ -71,51 +71,7 @@ def get_coordinates(path_to_image: str):
 
         bufferedDimensions.append([x,y,w,h])
     return bufferedDimensions
-    
-def get_coordinates(path_to_image: str):
-    """Returns the coordinates of the shapes in the image"""
 
-    LOWER_BLUE_COLOR = [25,25,0]
-    UPPER_BLUE_COLOR = [255,255,255]
-    CONTOUR_SIZE_RESTRICTION = 40
-    BORDER_SHAPE_ADD = 15
-
-    img = cv2.imread(path_to_image)
-    imghsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    lower_blue = np.array(LOWER_BLUE_COLOR) 
-    upper_blue = np.array(UPPER_BLUE_COLOR)
-    mask_blue = cv2.inRange(imghsv, lower_blue, upper_blue)
-    #Show masked image
-    # cv2.imshow("winname" , mask_blue)
-    contours, _ = cv2.findContours(mask_blue, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    im = np.copy(img)
-    imSizeRestrict = np.copy(img)
-    dimensionsShapes = []
-    for c in contours:
-        if (cv2.contourArea(c) > CONTOUR_SIZE_RESTRICTION): 
-            rect = cv2.boundingRect(c)
-            x,y,w,h = rect
-            dimensionsShapes.append([x,y,w,h])
-    # Checks each value with all others in arrary to see intersection, removes smaller array
-    for shape in dimensionsShapes:
-        for shapeCheck in dimensionsShapes:
-            if intersects((shape[0],shape[1],shape[2]+shape[0],shape[3]+shape[1]), (shapeCheck[0],shapeCheck[1],shapeCheck[2]+shapeCheck[0],shapeCheck[3]+shapeCheck[1])) and shape != shapeCheck:
-                if shape[2]*shape[3] > shapeCheck[2]*shapeCheck[3]:
-                    dimensionsShapes.remove(shapeCheck)
-                else:
-                    dimensionsShapes.remove(shape)
-
-    textBuffer = 5
-
-    bufferedDimensions = []
-
-    for shapeFinalizaed in dimensionsShapes:
-        x, y, w, h = shapeFinalizaed
-        bufferedDimensions.append([x - BORDER_SHAPE_ADD, y - BORDER_SHAPE_ADD,
-            w + BORDER_SHAPE_ADD * 2, h + BORDER_SHAPE_ADD * 2])
-
-    return bufferedDimensions
 
 def get_shape(path_to_image, coordinate):
     """-1: Random, 0: Drum, 1: Piano Tile, 2: High Hat"""
@@ -206,3 +162,11 @@ def get_mapping():
     """Returns the areas_to_tunes. For testing"""
 
     return app.areas_to_tunes
+
+@app.get("/shapes/{file_name}")
+def get_shapes(file_name):
+    """Returns the tune for the coordinate clicked"""
+    
+    path_to_image = os.path.join(app.folder_path, file_name)
+    coordinates = get_coordinates(path_to_image)
+    return coordinates
--- test.py
+++ test.py
@@ -7,20 +7,54 @@ from app import app
 client = TestClient(app)
 
 def test_update():
-    resp = client.post("/update/1.jpg")
+    resp = client.post("/update/2.jpg")
 
-    # assert resp.status_code == 200
+def perform_detection(imageread):
 
-def get_shape():
-    # Coordinate: (x, y, w, h) = [1456, 303, 638, 466]
-    client.get("/getshape/")
+    #converting the input image to grayscale image using cvtColor() function
+    imagegray = cv2.cvtColor(imageread, cv2.COLOR_BGR2GRAY)
+    #using threshold() function to convert the grayscale image to binary image
+    _, imagethreshold = cv2.threshold(imagegray, 245, 255, cv2.THRESH_BINARY_INV)
+
+    #finding the contours in the given image using findContours() function
+    imagecontours, _ = cv2.findContours(imagethreshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #for each of the contours detected, the shape of the contours is approximated using approxPolyDP() function and the contours are drawn in the image using drawContours() function
+    # print(imagecontours)
+    for count in imagecontours:
+        epsilon = 0.01 * cv2.arcLength(count, True)
+        approximations = cv2.approxPolyDP(count, epsilon, True)
+        cv2.drawContours(imageread, [approximations], 0, (0), 3)
+        #the name of the detected shapes are written on the image
+        i, j = approximations[0][0] 
+        if len(approximations) == 3:
+            cv2.putText(imageread, "Triangle", (i, j), cv2.FONT_HERSHEY_COMPLEX, 1, 0, 2)
+        elif len(approximations) == 4:
+            cv2.putText(imageread, "Rectangle", (i, j), cv2.FONT_HERSHEY_COMPLEX, 1, 0, 2)
+        elif len(approximations) == 5:
+            cv2.putText(imageread, "Pentagon", (i, j), cv2.FONT_HERSHEY_COMPLEX, 1, 0, 2)
+        elif 6 < len(approximations) < 15:
+            cv2.putText(imageread, "Ellipse", (i, j), cv2.FONT_HERSHEY_COMPLEX, 1, 0, 2)
+        else:
+            cv2.putText(imageread, "Circle", (i, j), cv2.FONT_HERSHEY_COMPLEX, 1, 0, 2)
+    #displaying the resulting image as the output on the screen
+    cv2.imshow(str(len(approximations)), imageread)
+    cv2.waitKey(0)
+
+def perform_prediction(img):
+
+    imagegray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, imagethreshold = cv2.threshold(imagegray, 127, 255, cv2.THRESH_BINARY_INV)
+    cv2.imshow('bruh', imagethreshold)
+    cv2.waitKey(0)
 
 def show_shapes():
-    img = cv2.imread("ex-images/1.jpg")
+    file_name = "2.jpg"
 
-    x = [[803, 1561, 52, 245], [260, 1454, 521, 609], [683, 1436, 46, 37], [471, 1411, 214, 241], [1165, 1105, 552, 527], [1191, 1086, 82, 39], [1307, 1076, 69, 34], [2017, 398, 122, 43], [1456, 303, 638, 466], [210, 223, 624, 610], [1646, 111, 60, 49]]
+    coords = client.get("/shapes/" + file_name).json()
+    # print(coords.json())
+    img = cv2.imread("ex-images/" + file_name)
 
-    for shape in x:
+    for shape in coords[2:]:
         x1 = shape[0]
         y1 = shape[1]
         w = shape[2]
@@ -28,10 +62,27 @@ def show_shapes():
         x2 = x1 + w
         y2 = y1 + h
         croppedImg = img[y1:y2, x1:x2]
-        cv2.imshow('bruh', croppedImg)
-        cv2.waitKey(0) 
+        perform_prediction(croppedImg)
+        # cv2.imshow('bruh', croppedImg)
+        # cv2.waitKey(0)
 
     cv2.destroyAllWindows() 
 
-test_update()
+# test_update()
 show_shapes()
+# perform_prediction(cv2.imread("ex-images/2.jpg"))
+
+
+# import numpy as np
+# import cv2 as cv
+
+# img = cv.imread('ex-images/2.jpg')
+# imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# ret, thresh = cv.threshold(imgray, 127, 255, 0)
+# contours, _ = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+# cv.drawContours(img, contours, -1, (0,255,0), 3)
+
+# print(len(contours))
+# cv2.imshow('Contours', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
